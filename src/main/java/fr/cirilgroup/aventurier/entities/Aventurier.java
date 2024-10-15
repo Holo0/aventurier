00bafe3c70a9d08951bb7e17d8f572aca2272a9f
ewan <ewan.galin@gmail.com>
2024-10-15 22:54:50 +0200

    create Aventurier class with tests

---
+++ src/main/java/fr/cirilgroup/aventurier/entities/Aventurier.java
@@ -0,0 +1,43 @@
+package fr.cirilgroup.aventurier.entities;
+
+public class Aventurier {
+    private Integer coordonneeX;
+    private Integer coordonneeY;
+
+    public Aventurier(Integer x, Integer y) {
+        coordonneeX = x;
+        coordonneeY = y;
+    }
+
+    public void bouger(String direction) throws Exception {
+        switch (direction) {
+            case "S":
+                this.coordonneeY = this.coordonneeY - 1;
+                break;
+            case "N":
+                this.coordonneeY = this.coordonneeY + 1;
+                break;
+            case "E":
+                this.coordonneeX = this.coordonneeX + 1;
+                break;
+            case "O":
+                this.coordonneeX = this.coordonneeX - 1;
+                break;
+            default:
+                throw new Exception("Wrong direction entered");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "aventurier [coordonneeX=" + coordonneeX + ", coordonneeY=" + coordonneeY + "]";
+    }
+
+    public Integer getCoordonneeX() {
+        return coordonneeX;
+    }
+
+    public Integer getCoordonneeY() {
+        return coordonneeY;
+    }
+}
