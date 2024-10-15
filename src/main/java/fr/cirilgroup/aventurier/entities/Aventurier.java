7d5e6ae0c28507faff0c11101f75dae5e39643d5
ewan <ewan.galin@gmail.com>
2024-10-15 23:16:37 +0200

    delete useless signleton + change tests

--- src/main/java/fr/cirilgroup/aventurier/entities/Aventurier.java
+++ src/main/java/fr/cirilgroup/aventurier/entities/Aventurier.java
@@ -1,30 +1,22 @@
 package fr.cirilgroup.aventurier.entities;
 
 public final class Aventurier {
-    private static Aventurier aventurier;
 
     private Integer coordonneeX;
     private Integer coordonneeY;
 
-    private Aventurier(Integer x, Integer y) {
+    public Aventurier(Integer x, Integer y) {
         coordonneeX = x;
         coordonneeY = y;
-    }
-
-    public static Aventurier getAventurier(Integer x, Integer y) {
-        if (aventurier == null) {
-            aventurier = new Aventurier(x, y);
-        }
-        return aventurier;
     }
 
     public void bouger(String direction) throws Exception {
         switch (direction) {
             case "S":
-                this.coordonneeY = this.coordonneeY - 1;
+                this.coordonneeY = this.coordonneeY + 1;
                 break;
             case "N":
-                this.coordonneeY = this.coordonneeY + 1;
+                this.coordonneeY = this.coordonneeY - 1;
                 break;
             case "E":
                 this.coordonneeX = this.coordonneeX + 1;
--- src/test/java/fr/cirilgroup/aventurier/entities/AventurierTest.java
+++ src/test/java/fr/cirilgroup/aventurier/entities/AventurierTest.java
@@ -9,53 +9,37 @@ public class AventurierTest {
 
     public AventurierTest() {
     }
- 
+
     @Before
     public void setUp() {
-       this.aventurier = Aventurier.getAventurier(0, 0);
+        this.aventurier = new Aventurier(0, 0);
     }
- 
-    @Test
-    public void testGetAventurier() {
-       Assert.assertNotNull(this.aventurier);
-       Assert.assertEquals(this.aventurier, Aventurier.getAventurier(0, 0));
-    }
- 
     @Test
     public void testBougerNord() throws Exception {
-       this.aventurier.bouger("N");
-       Assert.assertEquals(Integer.valueOf(1), this.aventurier.getCoordonneeY());
-    }
- 
-    @Test
-    public void testBougerSud() throws Exception {
-       this.aventurier.bouger("S");
-       Assert.assertEquals(Integer.valueOf(-1), this.aventurier.getCoordonneeY());
-    }
- 
-    @Test
-    public void testBougerEst() throws Exception {
-       this.aventurier.bouger("E");
-       Assert.assertEquals(Integer.valueOf(1), this.aventurier.getCoordonneeX());
-    }
- 
-    @Test
-    public void testBougerOuest() throws Exception {
-       this.aventurier.bouger("O");
-       Assert.assertEquals(Integer.valueOf(-1), this.aventurier.getCoordonneeX());
-    }
- 
-    @Test(
-       expected = Exception.class
-    )
-    public void testBougerWrongDirection() throws Exception {
-       this.aventurier.bouger("X");
+        this.aventurier.bouger("N");
+        Assert.assertEquals(Integer.valueOf(-1), this.aventurier.getCoordonneeY());
     }
 
     @Test
-   public void testSingleton() {
-      Aventurier aventurier1 = Aventurier.getAventurier(0, 0);
-      Aventurier aventurier2 = Aventurier.getAventurier(1, 1);
-      Assert.assertSame("Les instances devraient \u00eatre les m\u00eames", aventurier1, aventurier2);
-   }
+    public void testBougerSud() throws Exception {
+        this.aventurier.bouger("S");
+        Assert.assertEquals(Integer.valueOf(1), this.aventurier.getCoordonneeY());
+    }
+
+    @Test
+    public void testBougerEst() throws Exception {
+        this.aventurier.bouger("E");
+        Assert.assertEquals(Integer.valueOf(1), this.aventurier.getCoordonneeX());
+    }
+
+    @Test
+    public void testBougerOuest() throws Exception {
+        this.aventurier.bouger("O");
+        Assert.assertEquals(Integer.valueOf(-1), this.aventurier.getCoordonneeX());
+    }
+
+    @Test(expected = Exception.class)
+    public void testBougerWrongDirection() throws Exception {
+        this.aventurier.bouger("X");
+    }
 }
