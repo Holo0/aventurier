f783ccb95b2d5947858b9f609258636744503ba4
Ewan Galin <71515055+Holo0@users.noreply.github.com>
2024-10-21 23:11:07 +0200

    add documentation and Doxygen to project (#6)
    * add documentation in classes
    
    * add Doxygen in project
    
    * add another type for file name imput

--- src/main/java/fr/cirilgroup/aventurier/entities/Aventurer.java
+++ src/main/java/fr/cirilgroup/aventurier/entities/Aventurer.java
@@ -1,36 +1,71 @@
 package fr.cirilgroup.aventurier.entities;
 
+/**
+ * Class representing the adventurer.
+ */
 public final class Aventurer {
-
     private Integer x;
     private Integer y;
 
+    /**
+     * Constructor
+     *
+     * @param x The initial x-coordinate of the adventurer.
+     * @param y The initial y-coordinate of the adventurer.
+     */
     public Aventurer(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Moves the adventurer in the specified direction.
+     *
+     * @param direction The direction to move the adventurer.
+     */
     public void move(EnumDirection direction) {
         int[] move = getMove(direction);
         this.x += move[0];
         this.y += move[1];
     }
 
+    /**
+     * Gets the next position of the adventurer based on the direction.
+     *
+     * @param direction The direction to calculate the next position.
+     * @return The next position as an array of two integers [x, y].
+     */
     public int[] getNextPosition(EnumDirection direction) {
         int[] move = getMove(direction);
         return new int[] { x + move[0], y + move[1] };
     }
 
+    /**
+     * Gets the current x-coordinate of the adventurer.
+     *
+     * @return The current x-coordinate.
+     */
     public Integer getX() {
         return x;
     }
 
+    /**
+     * Gets the current y-coordinate of the adventurer.
+     *
+     * @return The current y-coordinate.
+     */
     public Integer getY() {
         return y;
     }
 
     // PRIVATE
 
+    /**
+     * Determines the difference of coordinate based on the direction.
+     *
+     * @param direction The direction to move.
+     * @return The move as an array of two integers [delta_x, delta_y].
+     */
     private int[] getMove(EnumDirection direction) {
         switch (direction) {
             case N:
--- src/main/java/fr/cirilgroup/aventurier/entities/Map.java
+++ src/main/java/fr/cirilgroup/aventurier/entities/Map.java
@@ -9,31 +9,59 @@ import java.util.List;
 
 import fr.cirilgroup.aventurier.Main;
 
+/**
+ * Class representing the game map.
+ */
 public class Map {
     private char[][] map;
 
+    /**
+     * Constructor
+     *
+     * @param mapFile The file containing the map.
+     * @throws IOException If there is an issue reading the map file.
+     */
     public Map(String mapFile) throws IOException {
         loadMap(mapFile);
     }
 
+    /**
+     * Checks if the specified position is valid on the map.
+     *
+     * @param x The x-coordinate to check.
+     * @param y The y-coordinate to check.
+     * @return True if the position is within bounds and not an impassable area,
+     *         false otherwise.
+     */
     public boolean isValidPosition(int x, int y) {
         // Check if the coordinates are within the boundaries of the map
         boolean withinBounds = y >= 0 && y < map.length && x >= 0 && x < map[0].length;
-        
+
         // Check if the target position is not an impassable area
         return withinBounds && map[y][x] == ' ';
     }
 
+    /**
+     * Gets the map as a 2D character array.
+     *
+     * @return The map.
+     */
     public char[][] getMap() {
         return map;
     }
 
     // PRIVATE
-    
+
+    /**
+     * Loads the map from the specified file.
+     *
+     * @param mapFile The file containing the map.
+     * @throws IOException If there is an issue reading the map file.
+     */
     private void loadMap(String mapFile) throws IOException {
         InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(mapFile);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        
+
         List<char[]> lines = new ArrayList<>();
         String line;
         while ((line = reader.readLine()) != null) {
